ae32a83d5f6cb41f08a145bbb8d74262946b0dd8
samuel <samuelmmanor@gmail.com>
2022-01-25 11:53:48 -0500

    added availiability function

--- index.js
+++ index.js
@@ -32,32 +32,64 @@ function displayCats(cats){
         catDescription.textContent = cat.Description
 
         //create like button
-        const catLikes = document.createElement('button')
-        catLikes.innerText = "Like me!"
-        catLikes.setAttribute('id', cat.id)
+        const catLikeButton = document.createElement('button')
+        catLikeButton.innerText = "Like me!"
+        catLikeButton.setAttribute('id', cat.id)
         const catLikeDisplay = document.createElement('p')
         catLikeDisplay.textContent = cat.likes + " likes"
-        catLikes.addEventListener('click', (cat) => {
+        catLikeButton.addEventListener('click', () => {
             //acess cat.likes and then when clicked updated catLikeDisplay
             //ADD IF STATEMENT FOR 1 LIKE AND THEN LIKES
             const newLikes = parseInt(catLikeDisplay.textContent) + 1
             catLikeDisplay.textContent = newLikes + " likes"
-            //potential: patch request to update server with newlikes
+            // const catLikes = parseInt(catLikeDisplay.textContent) + 1
+            // const catLikes = parseInt(catLikeDisplay.textContent) + 1
+            // if (catLikes > 1) {
+            //     catLikeDisplay.textContent = catLikes + " likes"
+            // } else {
+            //     catLikeDisplay.textContent = catLikes + " like"
+            // }
+
+
+            //potential: patch request to update server with like count
         })
 
         //create available button
         const catAvailability = document.createElement('button')
         catAvailability.innerText = "Check Availability"
         catAvailability.setAttribute('id', cat.Available)
-        catAvailability.addEventListener('click', (cat) => {
-            if (cat )
+        catAvailability.addEventListener('click', () => {
+            if (cat.Available) {
+                availabilityText.hidden = false
+                availabilityText.textContent = "Available! Enter your email here: "
+                // add email input space
+                const emailInput = document.createElement('input')
+                emailInput.setAttribute('type', 'email')
+                availabilityText.appendChild(emailInput)
+                // submit button
+                const emailSubmit = document.createElement('button')
+                emailSubmit.textContent = "Submit"
+                availabilityText.appendChild(emailSubmit)
+                emailSubmit.addEventListener('click', (e) => {
+                    availabilityText.textContent = "Thank you for submitting your email. We'll reach out to you soon!"
+                    emailSubmit.hidden = true
+                    // emailInput.value = ''
+                    emailInput.hidden = true
+                })
+            } else {
+                availabilityText.hidden = false
+                availabilityText.textContent = "Sorry, this cat is not available."
+            }
             //if avaiable add new reserver cat button and then click resever to grey out cat (and claim as yours!)
         })
+        const availabilityText = document.createElement('p')
+        availabilityText.textContent = ''
+        availabilityText.hidden = true;
 
         //add all above elements to catDiv
         //append catDiv to catContainer
 
-        catDiv.append(catName, catImage, catAge, catDescription, catLikeDisplay, catLikes, catAvailability)
+        catDiv.append(catName, catImage, catAge, catDescription, catLikeDisplay, catLikeButton, catAvailability, availabilityText)
         catContainer.append(catDiv)
 
     })
