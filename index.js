3cc6a73dd702db2d6fbdf59fa865a2c81cafb11e
Lindsey <lindsey.pregent@gmail.com>
2022-01-26 17:53:23 -0500

    JS cleaned up, html updated

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
 document.addEventListener('DOMContentLoaded',fetchCats)
+
 function fetchCats(){
     fetch("http://localhost:3000/cats")
     .then(response => response.json())
     .then(cats => displayCats(cats))
 }
 
+const newCatDiv = document.querySelector('#newCats')
+const newCatForm = document.querySelector(".newCatForm")
 const catContainer = document.querySelector('#catContainer')
+newCatForm.addEventListener('submit', addNewCat)
+
 function displayCats(cats){
     cats.forEach(cat => {
-        //create a div
         const catDiv = document.createElement('div')
         catDiv.className = "catCards"
 
-        //create h2 for name
         const catName = document.createElement('h2')
         catName.textContent = cat.name
 
-        //create image for image name
         const catImage = document.createElement('img')
         catImage.src = cat.image
-        // catImage.width = "300"
-        // catImage.height = "300"
         catImage.className = "catPictures"
 
-        //create p age
         const catAge = document.createElement('li')
         catAge.textContent = cat.age
 
-        //create p for description
         const catDescription = document.createElement('p')
         catDescription.textContent = cat.Description
 
-        //create like button
         const catLikeButton = document.createElement('button')
         catLikeButton.innerText = "Like me!"
         catLikeButton.className = "buttons"
@@ -39,23 +36,12 @@ function displayCats(cats){
         const catLikeDisplay = document.createElement('li')
         catLikeDisplay.textContent = cat.likes + " likes"
         catLikeButton.addEventListener('click', () => {
-            //acess cat.likes and then when clicked updated catLikeDisplay
-            //ADD IF STATEMENT FOR 1 LIKE AND THEN LIKES
+ 
             const newLikes = parseInt(catLikeDisplay.textContent) + 1
             catLikeDisplay.textContent = newLikes + " likes"
-            // const catLikes = parseInt(catLikeDisplay.textContent) + 1
-            // const catLikes = parseInt(catLikeDisplay.textContent) + 1
-            // if (catLikes > 1) {
-            //     catLikeDisplay.textContent = catLikes + " likes"
-            // } else {
-            //     catLikeDisplay.textContent = catLikes + " like"
-            // }
-
-
             //potential: patch request to update server with like count
         })
 
-        //create available button
         const catAvailability = document.createElement('button')
         catAvailability.className = "buttons"
         catAvailability.innerText = "Check Availability"
@@ -64,11 +50,11 @@ function displayCats(cats){
             if (cat.Available) {
                 availabilityText.hidden = false
                 availabilityText.textContent = "Available! Enter your email here: "
-                // add email input space
+    
                 const emailInput = document.createElement('input')
                 emailInput.setAttribute('type', 'email')
                 availabilityText.appendChild(emailInput)
-                // submit button
+
                 const emailSubmit = document.createElement('button')
                 emailSubmit.className = "emailSubmit"
                 emailSubmit.textContent = "Submit"
@@ -76,32 +62,23 @@ function displayCats(cats){
                 emailSubmit.addEventListener('click', (e) => {
                     availabilityText.textContent = "Thank you for submitting your email. We'll reach out to you soon!"
                     emailSubmit.hidden = true
-                    // emailInput.value = ''
                     emailInput.hidden = true
                 })
             } else {
                 availabilityText.hidden = false
                 availabilityText.textContent = "Sorry, this cat is not available."
             }
-            //if avaiable add new reserver cat button and then click resever to grey out cat (and claim as yours!)
         })
         const availabilityText = document.createElement('p')
         availabilityText.textContent = ''
         availabilityText.hidden = true;
         availabilityText.className = "availability"
 
-        //add all above elements to catDiv
-        //append catDiv to catContainer
-
         catDiv.append(catName, catImage, catAge, catDescription, catLikeDisplay, catLikeButton, catAvailability, availabilityText)
         catContainer.append(catDiv)
 
     })
 }
-const newCatDiv = document.querySelector('#newCats')
-const newCatForm = document.querySelector(".newCatForm")
-
-newCatForm.addEventListener('submit', addNewCat)
 
 function addNewCat(event) {
     event.preventDefault();
@@ -125,27 +102,26 @@ function addNewCat(event) {
     displayCatLocation.textContent = `Cat Seen: ${newCatLocation}`
     displayCatLocation.style.marginBottom = "100px"
 
-    const newCat = {
-        userName: document.querySelector("#userName").value,
-        newCatPicture: document.querySelector("#catPicture").value,
-        catName: document.querySelector("#catName").value,
-        newCatLocation: document.querySelector("#location").value
-    }
+    // const newCat = {
+    //     userName: document.querySelector("#userName").value,
+    //     newCatPicture: document.querySelector("#catPicture").value,
+    //     catName: document.querySelector("#catName").value,
+    //     newCatLocation: document.querySelector("#location").value
+    // }
 
-    addNewCatToServer(newCat)
-    // console.log(newCat)
+    // addNewCatToServer(newCat)
     newCatDiv.append(displayUserName, displayNewCatPic, displayNewCatName, displayCatLocation)
 }
 
-function addNewCatToServer(newCat) {
-    fetch("http://localhost:3000/userSubmittedCats", {
-        METHOD: 'POST',
-        Headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        Body: JSON.stringify(newCat)
-    })
-    .then(resp => resp.json())
-    .then(newCat => console.log(newCat))
-}
+// function addNewCatToServer(newCat) {
+//     fetch("http://localhost:3000/userSubmittedCats", {
+//         METHOD: 'POST',
+//         Headers: {
+//             'Content-Type': 'application/json',
+//             'Accept': 'application/json'
+//         },
+//         Body: JSON.stringify(newCat)
+//     })
+//     .then(resp => resp.json())
+//     .then(newCat => console.log(newCat))
+// }
